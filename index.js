9069f4ecf98007b32e410ab2ab1eafaf79decf93
Richard Wilson <rich@extream.app>
2020-11-19 21:03:25 +0000

    added callback error support

--- index.js
+++ index.js
@@ -83,6 +83,8 @@ const registerUsers = (requester, socketId, source, users, { itemId, instanceId,
       // need to send this user back as a status update for the socket
       if (callback) {
         await publish('ex-gateway', source, { domain: 'consumer', action: 'notification', command: 'update', payload: { itemId, instanceId, itemType, action: onComplete, user }, requester, socketId });
+      } else {
+        await publish('ex-gateway', source, { domain: 'consumer', action: 'notification', command: 'update', error: 'callback icomplete', payload: { itemId, instanceId, itemType, action: onComplete }, requester, socketId });
       }
     } catch (err) {
       await publish('ex-gateway', source, { domain: 'consumer', action: 'notification', command: 'update', error: err.message, payload: { itemId, instanceId, itemType, action: onComplete }, requester, socketId });
